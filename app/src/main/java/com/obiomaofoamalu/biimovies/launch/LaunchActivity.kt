f30c7adcaac5be8f0b8c39558d779ee3e69f1928
andela-oofoamalu <jessicaprincess61@gmail.com>
2018-03-06 19:27:59 +0100

    Initial Commit

---
+++ app/src/main/java/com/obiomaofoamalu/biimovies/launch/LaunchActivity.kt
@@ -0,0 +1,13 @@
+package com.obiomaofoamalu.biimovies.launch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.obiomaofoamalu.biimovies.R
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
