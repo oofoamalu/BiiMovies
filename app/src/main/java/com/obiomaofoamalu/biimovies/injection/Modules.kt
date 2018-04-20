c0fea213a74c8924fa94e912cedeba43ff046956
andela-oofoamalu <jessicaprincess61@gmail.com>
2018-04-20 21:00:27 +0100

    Setup remote data source connection

--- app/src/main/java/com/obiomaofoamalu/biimovies/injection/Modules.kt
+++ app/src/main/java/com/obiomaofoamalu/biimovies/injection/Modules.kt
@@ -2,10 +2,16 @@ package com.obiomaofoamalu.biimovies.injection
 
 import android.arch.persistence.room.Room
 import android.content.Context
+import android.content.res.Resources
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import com.obiomaofoamalu.biimovies.BiiApplication
 import com.obiomaofoamalu.biimovies.database.BiiDatabase
+import com.obiomaofoamalu.biimovies.service.BiiService
+import com.obiomaofoamalu.biimovies.service.ServiceInfoProvider
 import dagger.Module
 import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,7 +23,11 @@ class ApplicationModule(private val mApplication: BiiApplication) {
 
     @Provides
     @Singleton
-    fun providesContext(application: BiiApplication) = application.applicationContext
+    fun providesContext(application: BiiApplication): Context = application.applicationContext
+
+    @Provides
+    @Singleton
+    fun providesResources(context: Context): Resources = context.resources
 }
 
 @Module
@@ -25,6 +35,22 @@ class DatabaseModule {
 
     @Provides
     @Singleton
-    fun providesDatabase(context: Context) = Room.databaseBuilder(context, BiiDatabase::class.java,
+    fun providesDatabase(context: Context): BiiDatabase = Room.databaseBuilder(context, BiiDatabase::class.java,
             BiiDatabase.NAME).build()
+}
+
+@Module
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(serviceInfoProvider: ServiceInfoProvider): Retrofit = Retrofit.Builder()
+            .baseUrl(serviceInfoProvider.getBaseUrl())
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesBiiService(retrofit: Retrofit): BiiService = retrofit.create(BiiService::class.java)
 }
---
+++ app/src/main/java/com/obiomaofoamalu/biimovies/service/ServiceInfoProvider.kt
@@ -0,0 +1,12 @@
+package com.obiomaofoamalu.biimovies.service
+
+import android.content.res.Resources
+import com.obiomaofoamalu.biimovies.R
+import javax.inject.Inject
+
+class ServiceInfoProvider @Inject constructor (private val mResources: Resources) {
+
+    fun getBaseUrl() = mResources.getString(R.string.base_url)
+
+    fun getApiKey() = mResources.getString(R.string.api_key)
+}
