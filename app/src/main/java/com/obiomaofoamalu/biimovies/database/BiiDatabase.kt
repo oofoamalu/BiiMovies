8f8c882742ad7b71a4e67949700cc77882c81430
andela-oofoamalu <jessicaprincess61@gmail.com>
2018-04-20 11:56:33 +0100

    Add converter for list data

--- app/src/main/java/com/obiomaofoamalu/biimovies/database/BiiDatabase.kt
+++ app/src/main/java/com/obiomaofoamalu/biimovies/database/BiiDatabase.kt
@@ -2,8 +2,10 @@ package com.obiomaofoamalu.biimovies.database
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
+import android.arch.persistence.room.TypeConverters
 
 @Database(entities = arrayOf(Movie::class, Genre::class, Country::class), version = 1)
+@TypeConverters(Converters::class)
 abstract class BiiDatabase : RoomDatabase() {
 
     companion object {
--- app/src/main/java/com/obiomaofoamalu/biimovies/database/Models.kt
+++ app/src/main/java/com/obiomaofoamalu/biimovies/database/Models.kt
@@ -7,8 +7,8 @@ import android.arch.persistence.room.PrimaryKey
 @Entity(tableName = "Movies", indices = arrayOf(Index(value = "id", unique = true)))
 data class Movie(@PrimaryKey val id: Int, val title: String, val description: String,
                  val posterUrl: String, val duration: Int, val releaseYear: String,
-                 val rating: Double, val backdropUrl: String, val genreIds: MutableList<Int>,
-                 val countryIds: MutableList<String>)
+                 val rating: Double, val backdropUrl: String, val genreIds: ArrayList<Int>,
+                 val countryIds: ArrayList<String>)
 
 @Entity(tableName = "Genres", indices = arrayOf(Index(value = "id", unique = true)))
 data class Genre(@PrimaryKey val id: Int, val name: String)
