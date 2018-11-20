d8b0a37efdc5351f5afeb017b3607f0a7198a14a
andela-oofoamalu <jessicaprincess61@gmail.com>
2018-11-20 19:45:14 +0100

    Implement Launch Screen.

--- app/src/main/java/com/obiomaofoamalu/biimovies/launch/LaunchActivity.kt
+++ app/src/main/java/com/obiomaofoamalu/biimovies/launch/LaunchActivity.kt
@@ -2,12 +2,20 @@ package com.obiomaofoamalu.biimovies.launch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.obiomaofoamalu.biimovies.R
+import kotlinx.android.synthetic.main.activity_launch.*
 
+/**
+ * The [LaunchActivity] is the entry point of the application.
+ */
 class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+        browseMoviesButton.setOnClickListener {
+            Toast.makeText(this, "Implement me.", Toast.LENGTH_LONG).show()
+        }
     }
 }
